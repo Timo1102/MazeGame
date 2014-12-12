a125ec73d4965f2f1454a3c1406f7309e74999e1
Timo <Timo.1102@web.de>
2014-12-12 13:18:48 +0100

    add isActive to GameObject

--- GameHelper/GameObject.cs
+++ GameHelper/GameObject.cs
@@ -13,26 +13,56 @@ namespace GameHelper
 {
     public class GameObject : Microsoft.Xna.Framework.GameComponent
     {
+        public bool isActive;
+
         public Vector3 position;
         public Vector3 rotation;
         public Vector3 scale;
 
+        public Model model;
+
+        private Game game;
 
         public GameObject(Game game)
             : base(game)
         {
+            this.game = game;
             game.Components.Add(this);
+            isActive = true;
             position = new Vector3(0, 0, 0);
             rotation = new Vector3(0, 0, 0);
             scale = new Vector3(1.0f, 1.0f, 1.0f);
-
+           
         }
 
+        public void SetActive(bool active)
+        {
+            isActive = active;
+            
+        }
 
         public override void Update(GameTime gameTime)
         {
             Console.WriteLine("Muhahaha");
             base.Update(gameTime);
+        }
+
+        public void LoadModel(string name)
+        {
+            model = game.Content.Load<Model>(name);
+        }
+
+        public void SetEffects(Camera camera)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.Projection = camera.projectionMatrix;
+                    effect.View = camera.viewMatrix;
+                }
+            }
         }
 
     }
