686479db4249ed0ea95d3c892ed119f141f4c3ef
Timo <Timo.1102@web.de>
2014-12-12 18:12:47 +0100

    Update viewMatrix

--- GameHelper/Camera.cs
+++ GameHelper/Camera.cs
@@ -13,12 +13,13 @@ using Microsoft.Xna.Framework.Media;
 
 namespace GameHelper
 {
-
-
-
    public class Camera : GameObject
     {
         
+
+
+
+
        public Matrix projectionMatrix;
        public Matrix viewMatrix;
        public int i = 0;
@@ -41,10 +42,17 @@ namespace GameHelper
 
         public void SetMatrix()
         {
+            Vector3 cameraReference = new Vector3(0, 0, 1);
+            Matrix rotationMatrix = Matrix.CreateFromYawPitchRoll(rotation.X, rotation.Y, rotation.Z);
+            Vector3 transformedReference = Vector3.Transform(cameraReference, rotationMatrix);
+            Vector3 cameraLookat = position + transformedReference;
+
+
             projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
             MathHelper.ToRadians(45.0f),
             Game.GraphicsDevice.DisplayMode.AspectRatio, 1.0f, 10000.0f);
-            viewMatrix = Matrix.CreateLookAt(position, rotation, Vector3.Up);
+           // viewMatrix = Matrix.CreateLookAt(position, rotation, Vector3.Up);
+           viewMatrix = Matrix.CreateLookAt(position, cameraLookat, new Vector3(0.0f, 1.0f, 0.0f));
         }
 
     }
